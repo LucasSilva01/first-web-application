801d15ade758dcebc4bea8bf5c7557dc4201c685
LucasSilva01 <santoslucas9956@gmail.com>
2021-03-03 23:24:11 -0300

    adjective is received as a parameter and completes the sentence

---
+++ components/MariaPrea.js
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+const MariaPrea = ({palavra}) => (
+    <p> Maria preá é {palavra}</p>
+)
+export default MariaPrea
+
+
+
--- pages/maria.js
+++ pages/maria.js
@@ -1,24 +1,26 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-
-function MariaPrea() {
-  return (
+import MariaPrea from '../components/MariaPrea'
+const Maria = () => (
+ 
     <div className={styles.container}>
+        
         <title>Página Maria Preá</title>    
         <h1 className ={styles.description}>Página Maria Preá</h1>
         <ul>
             <div>
+                <MariaPrea palavra = {"Velha"}></MariaPrea>
                 <Link href="/">
-                    <button className={styles.card}>Clique para saber o segredo</button>                
+                <button className={styles.card}>Voltar</button>
                 </Link>
-                
             </div>
         </ul>
     </div>    
-  )
-}
+  
+)
 
-export default MariaPrea
+export default Maria
+
 
 
 
